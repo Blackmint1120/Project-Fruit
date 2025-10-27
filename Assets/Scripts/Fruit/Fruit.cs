5bfde592c19a5907733e03bd02f29a37fcdb3bfa
Gyeom Chong <93380825+Blackmint1120@users.noreply.github.com>
2025-10-27 13:14:08 +0900

    delete unusing code in fruit.cs

--- Assets/Scripts/Fruit/Fruit.cs
+++ Assets/Scripts/Fruit/Fruit.cs
@@ -8,7 +8,7 @@ public class Fruit : MonoBehaviour
     [SerializeField] private int level;
     public int Level => level;
 
-    void Awake()
+    private void Awake()
     {
         var rb = GetComponent<Rigidbody2D>();
         rb.sleepMode = RigidbodySleepMode2D.NeverSleep;
@@ -18,20 +18,14 @@ public class Fruit : MonoBehaviour
         level = def.level;
         var sr = GetComponent<SpriteRenderer>();
 
-        // 스프라이트는 기본 원(circle) 사용, 색상만 변경
-        if (sr.sprite == null)
-            sr.sprite = Resources.GetBuiltinResource<Sprite>("UI/Skin/Background.psd");
-        // 또는 직접 circle.png 한 장 만들어 넣어도 OK
-    
         var c = def.color;
         c.a = 1f;
-        sr.color = c;// 완전 불투명 보장
+        sr.color = c;
 
         var rb = GetComponent<Rigidbody2D>();
         rb.mass = Mathf.Max(0.01f, def.mass);
         
-        float baseRadius = 0.5f;
-        float scale = def.radius / baseRadius;
+        var scale = def.radius / 0.5f;
         transform.localScale = new Vector3(scale, scale, 1f);
     }
 }
