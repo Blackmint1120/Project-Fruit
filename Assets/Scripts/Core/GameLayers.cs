58130952590bdca1b5ffacd7e39aead6e20380f8
Gyeom Chong <93380825+Blackmint1120@users.noreply.github.com>
2025-10-27 10:40:13 +0900

    Delete unusing code (GameLayer)

--- Assets/Scripts/Core/GameLayers.cs
+++
@@ -1,9 +0,0 @@
-public static class GameLayers
-{
-    public static int Fruit => UnityEngine.LayerMask.NameToLayer("Fruit");
-    public static int Walls => UnityEngine.LayerMask.NameToLayer("Walls");
-    public static int UIBlock => UnityEngine.LayerMask.NameToLayer("UIBlock");
-
-    public static int MaskBlockingFruit =>
-        (1 << Walls) | (1 << Fruit); // 필요 시 확장
-}
