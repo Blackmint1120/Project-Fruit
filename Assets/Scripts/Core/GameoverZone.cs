2ee3ad70272396237d60d533975420dbaf0d4819
Gyeom Chong <93380825+Blackmint1120@users.noreply.github.com>
2025-10-27 10:46:39 +0900

    Update Gameover code
    - modify trigger time 1.5 to 3
    - make log every 1 second

--- Assets/Scripts/Core/GameoverZone.cs
+++ Assets/Scripts/Core/GameoverZone.cs
@@ -2,25 +2,32 @@ using UnityEngine;
 
 public class GameOverZone : MonoBehaviour
 {
-    [SerializeField] private float triggerTime = 1.5f; // 일정 시간 유지 시 게임오버
+    [SerializeField] private float triggerTime = 3f; // 일정 시간 유지 시 게임오버
     private float timer = 0f;
 
-    void OnTriggerStay2D(Collider2D col)
+    private int timerlog = 0;
+
+    private void OnTriggerStay2D(Collider2D col)
     {
         if (!col.CompareTag("Fruit")) return;
         timer += Time.deltaTime;
-        
-        Debug.Log("Gameoverzone detected : time = " + timer);
-        
-        if (timer >= triggerTime)
+
+        if (timer >= timerlog + 1)
         {
-            GameManager.Instance.GameOver();
+            Debug.Log($"{3 - timerlog}...");
         }
+
+        if (timer < triggerTime) return;
+        
+        // Game Over
+        timerlog = 0; 
+        GameManager.Instance.GameOver();
     }
 
-    void OnTriggerExit2D(Collider2D col)
+    private void OnTriggerExit2D(Collider2D col)
     {
         if (!col.CompareTag("Fruit")) return;
+        timerlog = 0;
         timer = 0f; // 빠져나가면 초기화
     }
 }
